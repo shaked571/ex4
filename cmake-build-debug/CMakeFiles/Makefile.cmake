b0930d8dfba3ef42ec4a78fcf978f4e7442345dd
shaked571 <shakedgreenfeld@gmail.com>
2017-06-09 19:41:53 +0300

    ss

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -44,5 +44,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/exercise2.dir/DependInfo.cmake"
+  "CMakeFiles/ex4.dir/DependInfo.cmake"
   )
